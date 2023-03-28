28c0121374b92f3724b6174222720f7d41c3c5f9
Kopo Ntheledi <kopontheledi@gmail.com>
2023-03-28 12:05:49 +0200

    bcl2302_Benjamin_KopoNtheledi_IWA_02

---
+++ IWA2 Submission/src/challenge1/challenge_1.js
@@ -0,0 +1,15 @@
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ 
+
+const surname = 'Smith'
+
+
+console.log(user, surname);
+
+/*comments were not written properly and also there were semicolon in the console.log instead of
+a comma sign*/
+//there was no other way to solve the code
---
+++ IWA2 Submission/src/challenge3/challenge_3.js
@@ -0,0 +1,22 @@
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn('Security scan starting')
+
+/* It is important to let user know when they can close the page */
+
+console.info('Please wait for scan to complete before closing the browser.');
+
+
+
+/*line 8 the string did not have '' inverted commas */
+/* the first comment should be a multi-line comment instead of a single-line comment so that
+it can comment everything */
+
+/* there si a typing erroe on line 12, its cnosole instead of console */
+
+/* on line 10, the multiline comment is not written properly, ends with forward slash star instead of star forward slash */
