b4082903a0864df844ac2d3cecb0eaf4b3914a4f
Kopo Ntheledi <kopontheledi@gmail.com>
2023-04-03 14:01:02 +0200

    bcl2302_Benjamin_KopoNtheledi_IWA5

--- IWA5/script.js
+++ IWA5/script.js
@@ -1,41 +1,40 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = 0
 
-if (location = RSA) {
-	shipping === 400 && currency === 'R'
+let locations = 'RSA'
+let currency = 'R'
+
+let shoes = 300 * 1;
+let toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED;
+let batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED;
+
+let shipping;
+
+if (locations === 'RSA') {
+  shipping = 400;
+} else if (locations === 'NAM') {
+  shipping = 600;
+} else if (locations === 'NK') {
+  console.log(BANNED_WARNING);
+} else {
+  shipping = 800;
 }
 
-if (location = NAM) { 
-shipping = 600 }
-else {
-	shipping = 800
+let subtotal = shoes + toys + shirts + batteries + pens;
+
+if (subtotal >= 1000 && locations !== 'NK') {
+  if (location === 'RSA' || (locations === 'NAM' && customers === 1)) {
+    shipping = 0;
+  } else {
+    console.log(FREE_WARNING);
+  }
 }
 
-
-	
-
-shoes = 300 * 1
-toys - 100 * 5
-hirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED'
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-		if (location = RSA)
-			shipping = 0 || calcShipping
-	}
+if (shipping === 0 && customers !== 1) {
+  console.log(FREE_WARNING);
+} else {
+  console.log('Price:', currency, subtotal + shipping);
 }
-}
-
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
-
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-const location = 'RSA'
-currency = null
