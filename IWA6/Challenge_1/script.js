22d4314d2b4cd0b2162b67765875b52f1645177a
Kopo Ntheledi <kopontheledi@gmail.com>
2023-04-04 15:46:59 +0200

    bcl2302_Benjamin_KopoNtheledi_IWA6

---
+++ IWA6/Challenge_1/script.js
@@ -0,0 +1,30 @@
+// const primaryPhone = 'O748105141'
+// const secondaryPhone = '0219131568'
+
+// // Only change below this line
+
+// const primaryValid = typeof primaryPhone == parseInt(primaryPhone)
+// const secondaryValid = typeof secondaryPhone == parseInt(secondaryPhone)
+
+// console.log('Primary phone is valid numerical string:', primaryValid)
+// console.log('Secondary phone is valid numerical string:', secondaryValid )
+
+
+
+
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = typeof primaryPhone === 'string' && !isNaN(primaryPhone)
+const secondaryValid = typeof secondaryPhone === 'string' && !isNaN(secondaryPhone)
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
+
+
+/*The code above checks if both primaryPhone and secondaryPhone are strings and 
+that they only contain numerical characters.
+ We use the typeof operator to check that the type of the variable is a string, 
+and isNaN function to check that the string only contains numerical characters.*/
---
+++ IWA6/Challenge_2/script.js
@@ -0,0 +1,35 @@
+
+const rent = 400;
+const tax = '12%'; 
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00; // hourOfDay and minuteOfDay should be numbers, not strings
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if (hourOfDay === 0 && minuteOfDay === 0) {
+    const taxAsDecimal = parseFloat(tax) / 100;
+	const startingAfterTax = salary * (1 - taxAsDecimal); // Calculate starting balance after tax
+	const balance = startingAfterTax - transport - food - rent; // Calculate final balance
+	console.log(balance.toFixed(2));
+}
+
+
+// const rent = 400;
+// const tax = '12%';
+// const food = 51.7501;
+// const salary = 800;
+// const transport = 10.2;
+// const hourOfDay = 0;
+// const minuteOfDay = 0;
+
+// // Only change below this line
+
+// if (hourOfDay === 0 && minuteOfDay === 0) {
+//   const taxAsDecimal = parseInt(tax) / 100;
+//   const startingAfterTax = salary * (1 - taxAsDecimal);
+//   const balance = startingAfterTax - transport - food - rent;
+//   console.log('R' + balance.toFixed(2));
+// }
