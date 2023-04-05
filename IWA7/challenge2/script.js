a2d34664da50d72f3e2f0d141df24fba0d9673e0
Kopo Ntheledi <kopontheledi@gmail.com>
2023-04-05 15:09:55 +0200

    bcl2302_Benjamin_KopoNtheledi_IWA7

---
+++ IWA7/challenge2/script.js
@@ -0,0 +1,14 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+
+if (nickname) {
+    console.log (`Good morning, ${nickname}!`);
+}  else if (firstname) {
+    console.log (`Good morning, ${firstname}!`);
+} else {
+    console.log("Good Morning!");
+}
+
+
+
---
+++ IWA7/challenge3/script.js
@@ -0,0 +1,26 @@
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+
+const divider = '----------------------------------'
+
+
+// Only change below this line
+
+const owed = parseFloat(leoBalance) + parseFloat(sarahBalance)
+const leo = `${leoName} ${leoSurname} Owed: R ${(-1 * parseFloat(leoBalance)).toFixed(2)}`
+const sarah = `${sarahName} ${sarahSurname} Owed: R ${(-1 * parseFloat(sarahBalance)).toFixed(2)}`
+const total = `  Total amount owed: R ${owed.toFixed(2)}`
+const result = `\n${leo}\n${sarah}\n\n${divider}\n${total}\n${divider}`
+
+console.log(result)
+
+
+/*We can use the .toFixed(2) method to format the numbers with 2 decimal places. This method returns a string representation 
+of the number with the specified number of decimal places.
+Note that we need to multiply the balance values by -1 to make them positive, since the .toFixed() method doesn't work with negative numbers.
+the \n newline character to format the output.*/
