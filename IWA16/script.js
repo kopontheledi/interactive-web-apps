da82d1fbbbe801c67ec79a22336e24bcda93c216
Kopo Ntheledi <kopontheledi@gmail.com>
2023-04-26 11:06:14 +0200

    add comments

--- IWA16/script.js
+++ IWA16/script.js
@@ -66,40 +66,50 @@ const MONTHS = [
   };
   
   // Only edit below this comment
-  const createHtml = (athlete) => {
-    //  reassign the variables
-    const firstName = data.response.data[athlete].firstName;
-    const surname = data.response.data[athlete].surname;
-    const id = data.response.data[athlete].id;
-    const races = (data.response.data[athlete].races).length
-    const date = new Date(data.response.data[athlete].races[races-1].date)
-    const time = data.response.data[athlete].races[races-1].time;
-    const fragment = document.createDocumentFragment();
-    let title = document.createElement('h2');
-    title.textContent = id
-    fragment.appendChild(title);
-    const list = document.createElement('dl');
-    const day =  date.getDate()
-    const month = MONTHS[date.getMonth()];
-    const year = date.getFullYear();
-    console.log(month)
-    let sum = 0
-    let i = 0;
-    while ( i < time.length ) {
-      sum += time[i]
-      i++
-    }
-    const minutes = sum % 60;
-    const hours = (sum - minutes) / 60;
-    list.innerHTML = /* html */ `
-        <dt>Athlete: ${firstName +' '+ surname}</dt>
-        <dt>Total Races: ${races}</dt>
-        <dt>Event Date (Latest): ${day.toString().padStart(2, '0')+' '+ month +' '+ year}</dt>
-        <dt>Total Time (Latest): ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}</dt>
-      `;
-    fragment.appendChild(list);
-    return fragment;
+const createHtml = (athlete) => {
+  //  reassign the variables
+  const firstName = data.response.data[athlete].firstName;
+  const surname = data.response.data[athlete].surname;
+  const id = data.response.data[athlete].id;
+  const races = (data.response.data[athlete].races).length
+  const date = new Date(data.response.data[athlete].races[races-1].date)
+  const time = data.response.data[athlete].races[races-1].time;
+
+  const fragment = document.createDocumentFragment();
+
+  let title = document.createElement('h2');
+  title.textContent = id
+  fragment.appendChild(title);
+
+  const list = document.createElement('dl');
+
+  const day =  date.getDate()
+  const month = MONTHS[date.getMonth()];
+  const year = date.getFullYear();
+  console.log(month)
+
+  //adds up all the elements in the array to get the total minutes on the specific race day
+  let sum = 0
+  let i = 0;
+  while ( i < time.length ) {
+    sum += time[i]
+    i++
   }
-  // [NM372], [SV782] = data
-  document.querySelector('[data-athlete = "NM372"]').appendChild(createHtml('NM372'));
-  document.querySelector('[data-athlete ="SV782"]').appendChild(createHtml('SV782'));
+
+  //assigns minutes to the remainder of the total time divide by 60
+  const minutes = sum % 60;
+  const hours = (sum - minutes) / 60;
+
+  list.innerHTML = /* html */ `
+      <dt>Athlete: ${firstName +' '+ surname}</dt>
+      <dt>Total Races: ${races}</dt>
+      <dt>Event Date (Latest): ${day.toString().padStart(2, '0')+' '+ month +' '+ year}</dt>
+      <dt>Total Time (Latest): ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}</dt>
+    `;
+  fragment.appendChild(list);
+
+  return fragment;
+}
+// [NM372], [SV782] = data
+document.querySelector('[data-athlete = "NM372"]').appendChild(createHtml('NM372'));
+document.querySelector('[data-athlete ="SV782"]').appendChild(createHtml('SV782'));
